b95e4eae5f500a760bd7141af316c7de6cfcc5aa
satchapum <satchapum.mail@mail.kmutt.ac.th>
2024-08-22 15:56:53 +0700

    FEAT: Change Player position

--- Assets/Assets/Script/SpawnScript/SpawnPosition.cs
+++ Assets/Assets/Script/SpawnScript/SpawnPosition.cs
@@ -2,7 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpawnPosition : MonoBehaviour
+public class SpawnPosition : Singleton<SpawnPosition>
 {
-    
+    [SerializeField] List<GameObject> spawnPosition;
+    [SerializeField] GameObject playerObject;
+
+    private void Start()
+    {
+        setPosition();
+    }
+
+    public void setPosition()
+    {
+        int currentMiniGame = GameManager.Instance.currentPlayerMiniGame;
+        
+        for (int numberOfSpawnPoint = 0; numberOfSpawnPoint < spawnPosition.Count; numberOfSpawnPoint++)
+        {
+            GameObject positionNumber = spawnPosition[numberOfSpawnPoint];
+            int positionMiniGame = positionNumber.GetComponent<SpawnPointNumber>().spawnPointNumber;
+
+            if (currentMiniGame == positionMiniGame)
+            {
+                playerObject.transform.position = positionNumber.transform.position;
+                playerObject.transform.rotation = positionNumber.transform.rotation;
+            }
+        }
+    }
 }
