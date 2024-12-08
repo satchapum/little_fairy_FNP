e3b533affda0f56f7738435afddc007a38cb8374
Satchapum <satchapum.mail@mail.kmutt.ac.th>
2024-12-08 22:36:50 +0700

    FIX: Font at bedroom and waste sorting

--- Assets/Assets/Script/WasteSorting/WasteBin.cs
+++ Assets/Assets/Script/WasteSorting/WasteBin.cs
@@ -42,9 +42,12 @@ public class WasteBin : MonoBehaviour
                 {
                     WasteManager.Instance.numberOfGeneralWaste--;
                 }
-
-                WasteManager.Instance.source.clip = WasteManager.Instance.clip_Pass;
-                WasteManager.Instance.source.Play();
+                if () 
+                {
+                    WasteManager.Instance.source.clip = WasteManager.Instance.clip_Pass;
+                    WasteManager.Instance.source.Play();
+                }
+                
             }
 
             else 
