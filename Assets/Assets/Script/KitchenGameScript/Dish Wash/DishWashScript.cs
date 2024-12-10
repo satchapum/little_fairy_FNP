90932078b22cf0f5a7d8928319f3f5cab291c2b3
Satchapum <satchapum.mail@mail.kmutt.ac.th>
2024-12-10 23:56:30 +0700

    ADD: change name of texture coord

--- Assets/Assets/Script/KitchenGameScript/Dish Wash/DishWashScript.cs
+++ Assets/Assets/Script/KitchenGameScript/Dish Wash/DishWashScript.cs
@@ -53,10 +53,10 @@ public class DishWashScript : MonoBehaviour
         {
             Debug.Log(spongeRayCast.isHit + "  :  " + spongeRayCast.hitObject.name + " : " + this.gameObject.name);
             Debug.Log("Do delete texture");
-            Vector2 textureCoord = spongeRayCast.hitOut.textureCoord;
-            Debug.Log(textureCoord);
-            int pixelX = Mathf.FloorToInt(textureCoord.x * _templateDirtMask.width);
-            int pixelY = Mathf.FloorToInt(textureCoord.y * _templateDirtMask.height);
+            Vector2 textureCoords = spongeRayCast.hitOut.textureCoord;
+            Debug.Log(textureCoords);
+            int pixelX = Mathf.FloorToInt(textureCoords.x * _templateDirtMask.width);
+            int pixelY = Mathf.FloorToInt(textureCoords.y * _templateDirtMask.height);
             Debug.Log(_templateDirtMask.width +" : ");
             Debug.Log(_templateDirtMask.height);
             Debug.Log("-----------------------");
