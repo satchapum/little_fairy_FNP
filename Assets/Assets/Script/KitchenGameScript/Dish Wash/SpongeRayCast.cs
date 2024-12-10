68947f56c5e4ddd68695b150bc377bd246d0d05e
Satchapum <satchapum.mail@mail.kmutt.ac.th>
2024-12-10 23:54:09 +0700

    ADD: Debug

--- Assets/Assets/Script/KitchenGameScript/Dish Wash/SpongeRayCast.cs
+++ Assets/Assets/Script/KitchenGameScript/Dish Wash/SpongeRayCast.cs
@@ -41,6 +41,7 @@ public class SpongeRayCast : Singleton<SpongeRayCast>
                 hitOut = hit;
                 isHit = true;
                 hitObject = hit.collider.gameObject;
+
                 MeshCollider meshCollider = hit.collider as MeshCollider;
                 Debug.Log(meshCollider == null);
             }
